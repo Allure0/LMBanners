71ebb59a0a69422e0aeee85ecf63c2fce3633911
allure0 <asddavid@163.com>
2016-07-29 18:16:47 +0800

    Compatible Build.VERSION.SDK_INT < 15

--- lm_banner_library/src/main/java/com/allure/lbanners/transformer/ZoomOutPageTransformer.java
+++ lm_banner_library/src/main/java/com/allure/lbanners/transformer/ZoomOutPageTransformer.java
@@ -2,6 +2,8 @@ package com.allure.lbanners.transformer;
 
 import android.view.View;
 
+import com.nineoldandroids.view.ViewHelper;
+
 /**
  * Created by lm on 2016/7/8.
  */
@@ -18,7 +20,7 @@ public class ZoomOutPageTransformer extends LMPageTransformer {
 
         if (position < -1) { // [-Infinity,-1)
             // This page is way off-screen to the left.
-            view.setAlpha(0);
+            ViewHelper.setAlpha(view,0);
         } else if (position <= 1) { // [-1,1]
             // Modify the default slide transition to
             // shrink the page as well
@@ -26,19 +28,22 @@ public class ZoomOutPageTransformer extends LMPageTransformer {
             float vertMargin = pageHeight * (1 - scaleFactor) / 2;
             float horzMargin = pageWidth * (1 - scaleFactor) / 2;
             if (position < 0) {
-                view.setTranslationX(horzMargin - vertMargin / 2);
+                ViewHelper.setTranslationX(view,horzMargin - vertMargin / 2);
             } else {
-                view.setTranslationX(-horzMargin + vertMargin / 2);
+                ViewHelper.setTranslationX(view,-horzMargin + vertMargin / 2);
             }
             // Scale the page down (between MIN_SCALE and 1)
-            view.setScaleX(scaleFactor);
-            view.setScaleY(scaleFactor);
+            ViewHelper.setScaleX(view,scaleFactor);
+            ViewHelper.setScaleY(view,scaleFactor);
+
+
             // Fade the page relative to its size.
-            view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE)
+            ViewHelper.setAlpha(view,MIN_ALPHA + (scaleFactor - MIN_SCALE)
                     / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.
-            view.setAlpha(0);
+            ViewHelper.setAlpha(view,0);
         }
     }
 
--- lm_banner_library/src/main/java/com/allure/lbanners/transformer/vertical/VerticalPageTransformer.java
+++ lm_banner_library/src/main/java/com/allure/lbanners/transformer/vertical/VerticalPageTransformer.java
@@ -3,6 +3,8 @@ package com.allure.lbanners.transformer.vertical;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.nineoldandroids.view.ViewHelper;
+
 /**
  * Created by lm on 2016/7/8.
  */
@@ -13,21 +15,18 @@ public class VerticalPageTransformer implements ViewPager.PageTransformer {
 
         if (position < -1) { // [-Infinity,-1)
             // This page is way off-screen to the left.
-            view.setAlpha(0);
+            ViewHelper.setAlpha(view, 0);
 
         } else if (position <= 1) { // [-1,1]
-            view.setAlpha(1);
-
+            ViewHelper.setAlpha(view, 1);
             // Counteract the default slide transition
-            view.setTranslationX(view.getWidth() * -position);
-
+            ViewHelper.setTranslationX(view, view.getWidth() * -position);
             //set Y position to swipe in from top
             float yPosition = position * view.getHeight();
-            view.setTranslationY(yPosition);
-
+            ViewHelper.setTranslationY(view,yPosition);
         } else { // (1,+Infinity]
             // This page is way off-screen to the right.
-            view.setAlpha(0);
+            ViewHelper.setAlpha(view, 0);
         }
     }
 }
